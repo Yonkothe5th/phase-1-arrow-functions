8afd81b381eaba8fbe4fd20044d69fdce1f31eea
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-10-04 09:25:29 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = (num1 = 2000 ,num2 = 100) => {
+    let NaN = num1 / num2;
+    return NaN;
+}
+divide ();
+
+const square =  (num1) => {
+    let x = 2;
+    return x * x ;
+}
+
+square ();
+
+const add = (num1 = 3,num2 = 4) => {
+    let NaN = num1 + num2
+    return NaN;
+}
+add ();
